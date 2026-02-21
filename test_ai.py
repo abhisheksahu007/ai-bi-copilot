51d6e10d1aae28c8cb84337249cb29aa12c91d41
abhisheksahu007 <abhisheksahu7225@gmail.com>
2026-02-21 11:57:29 +0530

    removed secrets

--- test_ai.py
+++ test_ai.py
@@ -1,7 +1,7 @@
 from openai import AzureOpenAI
 
 client = AzureOpenAI(
-    api_key="mUioPGaWXpVytXwabznwffKEXYyRFYTijQcRim2zgGAfoVSkZO23JQQJ99CBACHYHv6XJ3w3AAAAACOGmOy9",
+    api_key=os.getenv("AZURE_OPENAI_KEY"),
     api_version="2024-02-01",
     azure_endpoint="https://abhis-mlv09ybo-eastus2.cognitiveservices.azure.com/"
 )
